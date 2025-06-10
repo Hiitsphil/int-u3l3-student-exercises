6cb69fa5d9d503d865c9077986322a1bbb031c93
Hiitsphil <philemonawork23@gmail.com>
2025-06-10 14:00:25 -0400

    3.3

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let button = document.querySelector("button");
                                     
 // 1. Declare a variable called `answer` that stores the p tag that has the class name `answer`.
 
-
+let answer=document.querySelector(".answer");
 
 button.addEventListener("click", function() {
+    answer.innerHTML="Spiderverse";
+
 // 2. Replace the value with a string with a name of a movie you enjoy.
 // 3. Uncomment the line of code below, run, and try the button.
 // answer.innerHTML="movie name goes here";
